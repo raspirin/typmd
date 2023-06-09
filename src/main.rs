d819ecbdd5290673beb869f3e78b5a96185c5014
raspirin <f1193968991@126.com>
2023-06-09 22:48:46 +0800

    feat: empty start/end tag

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ use std::io;
 use pulldown_cmark::escape::StrWrite;
 use pulldown_cmark::{Event, Tag};
 
-struct Typ<'a, I, W> {
+struct Typ<I, W> {
     iter: I,
     writer: W
 }
 
-impl<'a, I, W> Typ<'a, I, W>
+impl<'a, I, W> Typ<I, W>
 where
     I: Iterator<Item = Event<'a>>,
     W: StrWrite
@@ -18,11 +18,11 @@ where
         }
     }
 
-    pub fn run(&mut self, s: &str) -> io::Result<()> {
+    pub fn run(&mut self) -> io::Result<()> {
         while let Some(event) = self.iter.next() {
             match event {
-                Event::Start(_) => {}
-                Event::End(_) => {}
+                Event::Start(tag) => self.start_tag(tag),
+                Event::End(tag) => self.end_tag(tag),
                 Event::Text(_) => {}
                 Event::Code(_) => {}
                 Event::Html(_) => {}
@@ -38,6 +38,29 @@ where
     }
 
     fn start_tag(&mut self, tag: Tag<'a>) -> io::Result<()> {
+        match tag {
+            Tag::Paragraph => {}
+            Tag::Heading(_, _, _) => {}
+            Tag::BlockQuote => {}
+            Tag::CodeBlock(_) => {}
+            Tag::List(_) => {}
+            Tag::Item => {}
+            Tag::FootnoteDefinition(_) => {}
+            Tag::Table(_) => {}
+            Tag::TableHead => {}
+            Tag::TableRow => {}
+            Tag::TableCell => {}
+            Tag::Emphasis => {}
+            Tag::Strong => {}
+            Tag::Strikethrough => {}
+            Tag::Link(_, _, _) => {}
+            Tag::Image(_, _, _) => {}
+        }
+
+        Ok(())
+    }
+
+    fn end_tag(&mut self,  tag: Tag<'a>) -> io::Result<()> {
         match tag {
             Tag::Paragraph => {}
             Tag::Heading(_, _, _) => {}
