fc8665aa0609ccc9576b99916680c828eea6f6b5
raspirin <f1193968991@126.com>
2023-06-10 22:41:22 +0800

    feat: heading

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+use std::fs::File;
 use std::io;
+use std::io::Read;
 use pulldown_cmark::escape::StrWrite;
-use pulldown_cmark::{Event, Tag};
+use pulldown_cmark::{Event, Parser, Tag};
 
 struct Typ<I, W> {
     iter: I,
-    writer: W
+    writer: W,
+    end_newline: bool,
 }
 
 impl<'a, I, W> Typ<I, W>
@@ -15,22 +18,65 @@ where
     pub fn new(iter: I, writer: W) -> Self {
         Self {
             iter, writer,
+            end_newline: true,
         }
+    }
+
+    #[inline]
+    fn write(&mut self, s: &str) -> io::Result<()> {
+        self.writer.write_str(s)?;
+
+        if !s.is_empty() {
+            self.end_newline = s.ends_with('\n');
+        }
+
+        Ok(())
+    }
+
+    #[inline]
+    fn open_newline(&mut self) -> io::Result<()> {
+        self.end_newline = true;
+        self.writer.write_str("\n")
     }
 
     pub fn run(&mut self) -> io::Result<()> {
         while let Some(event) = self.iter.next() {
             match event {
-                Event::Start(tag) => self.start_tag(tag),
-                Event::End(tag) => self.end_tag(tag),
-                Event::Text(_) => {}
-                Event::Code(_) => {}
-                Event::Html(_) => {}
-                Event::FootnoteReference(_) => {}
-                Event::SoftBreak => {}
-                Event::HardBreak => {}
-                Event::Rule => {}
-                Event::TaskListMarker(_) => {}
+                Event::Start(tag) => self.start_tag(tag)?,
+                Event::End(tag) => self.end_tag(tag)?,
+                Event::Text(text) => self.write(&text)?,
+                Event::Code(text) => {
+                    self.write("`")?;
+                    self.write(&text)?;
+                    self.write("`")?;
+                }
+                Event::Html(text) => {
+                    // TODO: fix html
+                    self.write(&text)?;
+                }
+                Event::FootnoteReference(name) => {
+                    // TODO: fix footnote reference
+                    self.write(&format!("footnote_ref:{}", &name))?;
+                }
+                Event::SoftBreak => self.open_newline()?,
+                Event::HardBreak => {
+                    if self.end_newline {
+                        self.write("parbreak()")?;
+                    } else {
+                        self.write("\nparbreak()")?;
+                    }
+                }
+                Event::Rule => {
+                    if self.end_newline {
+                        self.write("pagebreak()")?;
+                    } else {
+                        self.write("\npagebreak()")?;
+                    }
+                }
+                Event::TaskListMarker(yes) => {
+                    // TODO: fix task list marker
+                    self.write(&format!("checkbox:{}", yes))?;
+                }
             }
         }
 
@@ -39,8 +85,23 @@ where
 
     fn start_tag(&mut self, tag: Tag<'a>) -> io::Result<()> {
         match tag {
-            Tag::Paragraph => {}
-            Tag::Heading(_, _, _) => {}
+            Tag::Paragraph => {
+                if !self.end_newline {
+                    self.write("\n")?;
+                }
+            }
+            Tag::Heading(level, _, _) => {
+                if self.end_newline {
+                    self.end_newline = false;
+                } else {
+                    self.write("\n")?;
+                }
+
+                for _ in 1..=(level as i32) {
+                    self.write("=")?;
+                }
+                self.write(" ")?;
+            }
             Tag::BlockQuote => {}
             Tag::CodeBlock(_) => {}
             Tag::List(_) => {}
@@ -60,10 +121,14 @@ where
         Ok(())
     }
 
-    fn end_tag(&mut self,  tag: Tag<'a>) -> io::Result<()> {
+    fn end_tag(&mut self, tag: Tag<'a>) -> io::Result<()> {
         match tag {
-            Tag::Paragraph => {}
-            Tag::Heading(_, _, _) => {}
+            Tag::Paragraph => {
+                self.write("\n")?;
+            }
+            Tag::Heading(_, _, _) => {
+                self.write("\n")?;
+            }
             Tag::BlockQuote => {}
             Tag::CodeBlock(_) => {}
             Tag::List(_) => {}
@@ -86,5 +151,15 @@ where
 
 
 fn main() {
-    println!("Hello, world!");
+    let mut file = File::open("test/in.md").unwrap();
+    let mut input = String::new();
+    file.read_to_string(&mut input).unwrap();
+
+    let parser = Parser::new(&input);
+
+    let mut output = String::new();
+    let mut tpy = Typ::new(parser, &mut output);
+    tpy.run().unwrap();
+
+    print!("{output}");
 }
