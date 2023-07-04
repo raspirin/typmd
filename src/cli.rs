68244b0197ae6e24edb88e110c2795a0b4ec47f4
aspirin <f1193968991@126.com>
2023-07-04 14:16:02 +0800

    sync

---
+++ src/cli.rs
@@ -0,0 +1,6 @@
+use clap::Parser;
+
+#[derive(Parser)]
+pub struct Cli {
+    pub file: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,9 @@
-use pulldown_cmark::{Options, Parser};
-use std::fs::File;
-use std::io::Read;
-use typmd::Typ;
+use typmd::cli::Cli;
+use clap::Parser;
 
 fn main() {
-    let mut file = File::open("test/in.md").unwrap();
-    let mut input = String::new();
-    file.read_to_string(&mut input).unwrap();
+    let cli = Cli::parse();
+    let path = cli.file;
 
-    let opt = Options::all();
-    let parser = Parser::new_ext(&input, opt);
 
-    let mut output = String::new();
-    let mut tpy = Typ::new(parser, &mut output);
-    tpy.run().unwrap();
-
-    print!("{output}");
 }
